6caa8fa45f35171aeea416d1da5fea900becf120
Shabin-k <helloshabink@gmail.com>
2023-04-01 12:41:33 +0530

    install sw

---
+++ public/serviceWorker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = [ 'index.html', 'offline.html' ];
+
+const self = this;
+
+//install sw
+self.addEventListener('install' , (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('opened cache');
+            return cache.addAll(urlsToCache)
+        })
+    )
+});
+//Listen for requests
+self.addEventListener('fetch' , (event) => {});
+//Active the sw
+self.addEventListener('active' , (event) => {});
